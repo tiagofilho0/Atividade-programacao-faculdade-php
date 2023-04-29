91912ac8a477c173564aeaa6477e749362bb1a92
tiagofilho0 <128102392+tiagofilho0@users.noreply.github.com>
2023-04-28 21:15:24 -0300

    Create Atividade1.for

---
+++ Atividade1.for
@@ -0,0 +1,21 @@
+# Atividade-programacao-faculdade-php
+
+<?php
+
+$valor = 100;
+#$valorc = $valor;
+
+if ($valor %2 == 0) {
+
+ for ($i=0; $i < $valor ; $i++) { 
+   #$valor = $valor - 1;
+    
+   echo($i."<br>");
+}
+}
+
+else {
+    echo('é impar');
+}
+
+?>
